482c00501a47ac28552dc12410c763bc3ad7d130
m-dorsey <dorseymakenzie@gmail.com>
2024-03-02 21:57:56 -0500

    fixed json and added team sites :)

--- src/components/TeamSites.jsx
+++ src/components/TeamSites.jsx
@@ -1,17 +1,31 @@
 
-const teams = ["angel city", "san diego wave", "bay fc"]
+import teams from '../data/json/teams.json'
+
+import '../assets/styles/Navigation.css'
+
+// const clubs = JSON.parse(teams)
+console.log(teams) 
 
 export const TeamSites = () => {
 
     return (
-        <>
+        <div className='team-site-header'>
             {
-                teams.map((t) => (
-                    <p style={{display:'inline'}}>{t} </p>
+                teams.map((team) => (
+                    <a className='team-site' target="_blank" href={team['team_website'] }
+                        key={team['team_id']} 
+                    >
+                        <img  
+                             src={team['team_badge']} 
+                             alt={team['team_abbreviation']} 
+                             title={team['team_name']} 
+                             className='team-badge'
+                        />
+                    </a>
                 ))
                 
             }
-        </>
+        </div>
     )
 }
 
